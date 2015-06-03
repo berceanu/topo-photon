0e9498c9db91817522a0686e9d006b0380c248d6
Andrei Berceanu <berceanu@users.noreply.github.com>
2015-06-03 22:21:40 +0200

    list comprehension for readability

--- anc/scripts/nonabelian.jl
+++ anc/scripts/nonabelian.jl
@@ -1,36 +1,23 @@
-import HH
-using Base.Test
+δE(1,11,1, linspace(-π/11, π/11, 20),linspace (-π, π, 20),0.02)
 
-#reload("HH")
+# -> Float64
+# calculates the average (over specified points) non-abelian energy correction to n(th) band
+# Note: MBZ:: (px, py) -> linspace(-π/q, π/q, N), linspace (-π, π, N)
+δE(n::Int,q::Int,p::Int, px::Array{Float64, 1},py::Array{Float64, 1},κ::Float64) = mean([δE(n,q,p, x,y,κ) for y in py, x in px])
+
+# -> Float64
+# calculates non-abelian energy correction to n(th) band at position (kₓ⁰,ky) in the MBZ
+δE(n::Int,q::Int,p::Int, k₀x::Float64,ky::Float64,κ::Float64) =  κ/2*sum([n′ != n ? norm(A(n,n′,q,p, k₀x,ky))^2 : 0.0 for n′ in 1:q])
 
 
-
-
-# () -> Float64
-# calculates the average (over the MBZ) non-abelian energy correction to n(th) band
-function δE(n::Int,q::Int,p::Int, px::Array{Float64, 1},py::Array{Float64, 1},κ::Float64)
-    Q = Array(Float64, length(py), length(px))
+# -> [Complex{Float64}, Complex{Float64}]
+# calculates Berry connection A (vector quantity) at position (kₓ⁰,ky) in the MBZ for bands n and n′
+function A(n::Int,n′::Int,q::Int,p::Int, k₀x::Float64,ky::Float64)
     # initializing hamiltonian matrices
     for M = (:H, :∇Hx, :∇Hy)
         @eval ($M) = zeros(Complex{Float64}, ($q,$q))
     end 
 
-    for (j,x) in enumerate(px), (i,y) in enumerate(py)
-        Q[i,j] = δE(n,q,p, x,y,κ)
-    end 
-    mean(Q)
-end 
-
-
-# () -> Float64
-# calculates non-abelian energy correction to n(th) band at position (kₓ⁰,ky) in the MBZ
-δE(n::Int,q::Int,p::Int, k₀x::Float64,ky::Float64,κ::Float64) =  κ/2*sum([n′ != n ? norm(A!(n,n′,q,p, k₀x,ky))^2 : 0.0 for n′ in 1:q])
-
-
-
-# () -> [Complex{Float64}, Complex{Float64}]
-# calculates Berry connection A (vector quantity) at position (kₓ⁰,ky) in the MBZ for bands n and n′
-function A!(n::Int,n′::Int,q::Int,p::Int, k₀x::Float64,ky::Float64)
     # top right corner
     H[1,q] = -exp(-im*q*ky)
     ∇Hy[1,q] = im*q*exp(-im*q*ky) # ∂ky
@@ -75,19 +62,23 @@ end
 
 
 
+#import HH
+using Base.Test
 
 
+function foo(px::Array{Float64, 1}, py::Array{Float64, 1})
+    Q = Array(Float64, length(py), length(px))
 
-#H = zeros(Complex{Float64}, (11,11));
-#HH.momsphhmat!(H, π/2, e/2)
+    for (j,x) in enumerate(px), (i,y) in enumerate(py)
+        Q[i,j] = x-y
+    end 
+    Q
+end 
+
+bazz(px::Array{Float64, 1}, py::Array{Float64, 1}) = [x - y for y in py, x in px]
 
 
-#@test_approx_eq A(1,3,11,1, π/2, e/2) H
+R = foo(linspace(1,10,10), linspace(1,5,5))
+S = bazz(linspace(1,10,10), linspace(1,5,5))
 
-#norm(A(1,3,11,1, π/2, e/2))^2
-
-
-
-δE(1,11,1, linspace(-π/11, π/11, 20),linspace (-π, π, 20),0.02)
-
-@allocated δE(1,11,1, linspace(-π/11, π/11, 20),linspace (-π, π, 20),0.02)
+@test_approx_eq R S
