8ddc7c0bcac3066a2f38f63faefbbc647559b240
Andrei Berceanu <berceanu@users.noreply.github.com>
2015-06-20 17:32:00 +0200

    added inset with zoomed view of \beta = 4 peak in homogeneous pumping, symmetric gauge

--- anc/scripts/selection_fig.jl
+++ anc/scripts/selection_fig.jl
@@ -1,4 +1,7 @@
 using PyPlot
+using PyCall
+@pyimport mpl_toolkits.axes_grid1.inset_locator as axloc
+
 import BP
 
 # system parameters
@@ -6,7 +9,7 @@ const N = 45
 const q = 11
 const κ = 0.02
 const γ = 0.001
-const ν = linspace(-3.45,-2.47,981);
+const ν = linspace(-3.45,-2.47,981)
 ##
 
 
@@ -15,7 +18,7 @@ exstates = BP.ExactStates(29, :landau, N, 1/q, κ)
 
 
 #for plotting filter markers
-βlan = [0,1,3,5]
+βlan = [0,2,4,6]
 βsym = [0,1,9,20]
 βreal = [0,6,15,26]
 
@@ -29,18 +32,24 @@ sω0lan = [exstates.νs[state]::Float64 for state in ηlan]
 sω0sym = [exstates.νs[state]::Float64 for state in ηsym]
 sω0real= [exstates.νs[state]::Float64 for state in ηreal]
 
+# energy boundaries for state with β=4
+ω₁= exstates.νs[4] - 0.005
+ω₂= exstates.νs[6] + 0.005
+
 
 δpmp(n₀::Int,m₀::Int) = BP.δpmp(N; n0=n₀, m0=m₀)
 gausspmp(n₀::Int,m₀::Int) = BP.gausspmp(N; σ=1., n0=n₀, m0=m₀)
 homopmp() = BP.homopmp(N)
 randpmp(s::Int) = BP.randpmp(N; seed=s) #1234
 
-           
 prm = (1/q,γ,κ);
 
 spδl = BP.Spectrum(ν,δpmp(5,5), :landau, prm...)
 spgaussl = BP.Spectrum(ν,gausspmp(5,5), :landau, prm...)
 sphoml = BP.Spectrum(ν,homopmp(), :landau, prm...)
+
+# centered gaussian
+# spgauss0 = BP.Spectrum(ν, BP.gausspmp(N; A=1., σ=20., n0=0, m0=0), :symmetric, prm...)
 
 
 spgausss = BP.Spectrum(ν,gausspmp(5,5), :symmetric, prm...)
@@ -98,7 +107,7 @@ matplotlib["rcParams"][:update](["axes.labelsize" => 22,
                                  "figure.autolayout" => true])
 
 
-f, axes = plt.subplots(4, figsize=(10, 7.3))
+fig, axes = plt.subplots(4, figsize=(10, 7.3))
 axes[1][:plot](spδl.νs,spδl.intensity,"k") 
 for ω in sω0real
     axes[1][:axvline](x = ω, color="k", ls="dotted")
@@ -119,6 +128,45 @@ axes[2][:yaxis][:set_ticklabels]([L"$0$", L"$10^3$"])
 
 axes[3][:plot](sphoml.νs,sphoml.intensity,"k") 
 axes[3][:plot](sphoms.νs,sphoms.intensity, color="green", ls="dashed", linewidth=1.5)
+# insert with zoom of peak β=4
+axins = axloc.inset_axes(axes[3],
+                        width="30%", # width = 30% of parent_bbox
+                        height="50%",
+                        loc=9) # located at upper middle part
+# plot same thing as in parent box
+axins[:plot](sphoms.νs,sphoms.intensity, color="green", ls="dashed", linewidth=1.5)
+# but set much narrower limits
+axins[:set_xlim]([ω₁, ω₂])
+axins[:xaxis][:set_ticks]([ω₁, ω₂])
+
+axins[:xaxis][:set_ticklabels]([string(round(ω₁,2)), string(round(ω₂,2))], fontsize=8)
+
+
+axins[:set_ylim]([1000, 10000])
+axins[:yaxis][:set_ticks]([1000, 10000])
+axins[:yaxis][:set_ticklabels]([L"$10^3$", L"$10^4$"], fontsize=8)
+
+
+
+# draw vertical lines at position of every exact eigenstate
+for ω in exstates.νs
+    axins[:axvline](x = ω, color="orange", ls="dotted")
+end 
+
+
+# transparency setting, not needed when exporting to pdf
+#axins[:patch][:set_alpha](1.0)
+
+# hide inset tick labels
+#plt.xticks(visible=false)
+#plt.yticks(visible=false)
+
+
+
+# draw a bbox of the region of the inset axes in the parent axes and
+# connecting lines between the bbox and the inset axes area
+axloc.mark_inset(axes[3], axins, loc1=2, loc2=4, ec="0.", fc="none")
+
 for ω in sω0lan
     axes[3][:axvline](x = ω, color="k", ls="dotted")
 end 
@@ -142,8 +190,23 @@ for (i, ax) in enumerate(axes)
     i != 4 && ax[:set_xticklabels]([])
 end 
 
-f[:savefig]("../../figures/selection.pdf", transparent=true, pad_inches=0.0, bbox_inches="tight")
-plt.close(f)
+fig[:savefig]("../../figures/selection.pdf", pad_inches=0.0, bbox_inches="tight")
+plt.close(fig)
 
-#TODO: add extra panel for centered gaussian, in linear scale with a zoom
 
+# plotting panel for centered gaussian
+## fig, ax = plt.subplots(figsize=(8, 3))
+##     ax[:plot](spgauss0.νs, spgauss0.intensity, color="green", linewidth=1.5)
+
+##     ax[:set_xlim](spgauss0.νs[1], spgauss0.νs[end])
+
+##     ax[:set_xlabel](L"$\omega_0 [J]$")
+##     ax[:set_yticklabels]([])
+
+## fig[:savefig]("../../figures/gaussian.pdf", transparent=true, pad_inches=0.0, bbox_inches="tight")
+## plt.close(fig)
+
+
+# TODO: add (a), (b), (c), (d) labels to subplots
+# TODO: add common y label to all subplots
+# TODO: add β labels to vertical lines in all subplots
