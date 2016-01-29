422b44617bdf7908ac888e8ab387ae793ee06266
Andrei Berceanu <berceanu@users.noreply.github.com>
2016-01-29 19:22:24 +0000

    Removed empty lines

--- anc/scripts/BP.jl
+++ anc/scripts/BP.jl
@@ -21,20 +21,14 @@ WaveFunction(S::SparseMatrixCSC{Complex{Float64},Int64},
              α::Float64,γ::Float64,κ::Float64) =
                  WaveFunction(S,ω,P,gauge,α,γ,κ, 0., 0.)
 
-
-
 function WaveFunction(S::SparseMatrixCSC{Complex{Float64},Int64},
                       ω::Float64,P::Vector{Complex{Float64}},gauge::Symbol,
                       α::Float64,γ::Float64,κ::Float64, m₀::Float64, n₀::Float64)
-
     N::Int = sqrt(length(P))
-
     eval(:($(symbol(string("buildham_", gauge, "!")))))(S, N,α,κ,γ,ω,m₀,n₀) 
     X = S\P
-
     return WaveFunction(N,sum(abs2(X)),X)
 end
-
 
 type ExactStates
     N::Int
@@ -44,30 +38,27 @@ type ExactStates
 end
 
 ExactStates(nev::Int, gauge::Symbol) = ExactStates(nev, gauge, 45)
-ExactStates(nev::Int, gauge::Symbol, N::Int) = ExactStates(nev, gauge, N, 1/11, 0.02, 0., 0.)
-ExactStates(nev::Int, gauge::Symbol, N::Int, α::Float64, κ::Float64) = ExactStates(nev, gauge, N, α, κ, 0., 0.)
+ExactStates(nev::Int, gauge::Symbol, N::Int) = ExactStates(nev, gauge,
+   N, 1/11, 0.02, 0., 0.)
+ExactStates(nev::Int, gauge::Symbol, N::Int, α::Float64, κ::Float64) =
+   ExactStates(nev, gauge, N, α, κ, 0., 0.)
 
-function ExactStates(nev::Int, gauge::Symbol, N::Int, α::Float64, κ::Float64, m₀::Float64, n₀::Float64)
+function ExactStates(nev::Int, gauge::Symbol, N::Int, α::Float64,
+   κ::Float64, m₀::Float64, n₀::Float64)
     M = spzeros(Complex{Float64}, N^2,N^2)
     eval(:($(symbol(string("buildhamexact", gauge, "!")))))(M, N,α,κ,m₀,n₀) 
-
     (d, v, nconv, niter, nmult, resid) = eigs(M, nev=nev, which=:SR, ritzvec=true)
-
     return ExactStates(N, gauge, real(d), v)
 end
 
 function getstate(s::ExactStates, ω::Float64)
     i::Int = indmin(abs(s.νs .- ω))
-
     return reshape(s.states[:,i], (s.N, s.N))
 end 
-
-
 
 function getstate(s::ExactStates, η::Int)
     return reshape(s.states[:,η], (s.N, s.N))
 end 
-
 
 type Spectrum
     N::Int
@@ -89,7 +80,6 @@ function Spectrum(ν::Vector{Float64},P::Vector{Complex{Float64}},gauge::Symbol)
         statevec[i] = WaveFunction(A, ω, P, gauge)
         intvec[i] = statevec[i].int
     end 
-
     return Spectrum(N, gauge, P, ν, intvec, statevec)
 end
 
@@ -103,10 +93,8 @@ function Spectrum(ν::Vector{Float64},P::Vector{Complex{Float64}},gauge::Symbol,
         statevec[i] = WaveFunction(A, ω, P, gauge, α,γ,κ)
         intvec[i] = statevec[i].int
     end 
-
     return Spectrum(N, gauge, P, ν, intvec, statevec)
 end
-
 
 function Spectrum(ν::Vector{Float64},P::Vector{Complex{Float64}},gauge::Symbol,
                   α::Float64,γ::Float64,κ::Float64, m₀::Float64, n₀::Float64)
@@ -118,10 +106,8 @@ function Spectrum(ν::Vector{Float64},P::Vector{Complex{Float64}},gauge::Symbol,
         statevec[i] = WaveFunction(A, ω, P, gauge, α,γ,κ, m₀,n₀)
         intvec[i] = statevec[i].int
     end 
-
     return Spectrum(N, gauge, P, ν, intvec, statevec)
 end
-
 
 function getstate(s::Spectrum, ω::Float64)
     i::Int = indmin(abs(s.νs .- ω))
@@ -141,14 +127,11 @@ function radius(M::Matrix{Float64}, axis::Vector)
     isodd(N) || throw(DimensionMismatch("invalid matrix size N=$N. N must be odd"))
     length(axis) == N || throw(DimensionMismatch("axis size must match matrix dimensions"))
     @test_approx_eq_eps(M, transpose(M), 1e-5)
-    
     half = div(N-1, 2) + 1
     v1 = axis[half:end]
     v2 = M[half:end, half]
-    
     i = indmax(v2)
     r = v1[i]
-
     return r
 end
 
@@ -184,8 +167,6 @@ macro hambody(fself, fleft, fright, fup, fdown)
     end 
 end 
 
-
-
 function buildham_landau!(S::SparseMatrixCSC{Complex{Float64},Int}, N::Int,α::Float64,κ::Float64,γ::Float64,ω::Float64, m₀::Float64, n₀::Float64)
     @hambody(ω + im*γ - 1/2*κ*((n-n₀)^2+(m-m₀)^2), 1, 1, exp(-im*2π*α*m), exp(im*2π*α*m))
 end
@@ -196,7 +177,6 @@ end
 
 buildham_exact!(S::SparseMatrixCSC{Complex{Float64},Int}, N::Int,α::Float64,κ::Float64) = buildham_exact!(S, N,α,κ, 0., 0.)
 
-
 function buildham_exact!(S::SparseMatrixCSC{Complex{Float64},Int}, N::Int,α::Float64,κ::Float64, m₀::Float64, n₀::Float64)
     @hambody(1/2*κ*((n-n₀)^2+(m-m₀)^2), -1, -1, -exp(-im*2π*α*m), -exp(im*2π*α*m))
 end
@@ -205,7 +185,6 @@ buildhamexactlandau! = buildham_exact!
 function buildhamexactsymmetric!(S::SparseMatrixCSC{Complex{Float64},Int}, N::Int,α::Float64,κ::Float64, m₀::Float64, n₀::Float64)
     @hambody(1/2*κ*((n-n₀)^2+(m-m₀)^2), -exp(-im*π*α*n), -exp(im*π*α*n), -exp(-im*π*α*m), -exp(im*π*α*m))
 end
-
 
 function genspmat(l::Function,r::Function,u::Function,d::Function,s::Function, N::Int,nz::Int,α::Float64)
     iseven(N) && throw(ArgumentError("invalid system size N=$N. N must be odd"))
@@ -232,10 +211,8 @@ function genspmat(l::Function,r::Function,u::Function,d::Function,s::Function, N
             J[k] = i; I[k] = i; V[k] = s(n,m,α)
         end
     end
-            
     # maximum value of m or n indices
     maxm = div(N-1,2)
-
     k = 0
     for i in 1:N^2
         m = getm(i,N)
@@ -286,7 +263,6 @@ function genspmat(l::Function,r::Function,u::Function,d::Function,s::Function, N
             setnzelem(i,n,m; pos="left")
         end
     end
-
     return sparse(I,J,V)
 end
 
@@ -324,7 +300,6 @@ end
 #arbitrary resolution fft
 function myfft2(ψr::Matrix{Complex{Float64}}, k1::Float64, k2::Float64, xs1::Float64, xs2::Float64, Δx1::Float64, Δx2::Float64)
     (N1,N2) = size(ψr)
-
     s = zero(Complex{Float64})
     for n2 in 1:N2, n1 in 1:N1
         xn1 = xs1 + (n2-1)*Δx1 #x
@@ -336,8 +311,6 @@ function myfft2(ψr::Matrix{Complex{Float64}}, k1::Float64, k2::Float64, xs1::Fl
 end
 function myfft2(ψr::Matrix{Complex{Float64}}, k1, k2)
     N1 = length(k2); N2 = length(k1)
-    
-
     out = Array(Complex{Float64}, N1,N2)
     for j in 1:N2, i in 1:N1
         out[i,j] = myfft2(ψr, k1[j], k2[i], 0., 0., 1., 1.)
@@ -355,10 +328,9 @@ end
 function mbz(data::Array{Float64,2}, r::Int, q::Int, kxmbz::FloatRange{Float64}, k::FloatRange{Float64})
     # data is |ψ(FBZ)|², input
     V = zeros(Float64, (length(k), r)) # |ψ(MBZ)|², output
-
-    for (i,pₓ) in enumerate(kxmbz) # loop over momenta in MBZ
+    for (i,px) in enumerate(kxmbz) # loop over momenta in MBZ
         for j = 0:q-1 # sum over various components
-            ptld = pₓ - j * 2π/q
+            ptld = px - j * 2π/q
             idx = indmin(abs(k .- fbz(ptld))) # index of \tilde{p_x}
             V[:,i] += data[:, idx]
         end
@@ -369,7 +341,7 @@ end
 #comparison to analytics
 function compute_hermite_polynomial(n)
     P = Poly([1])
-    const x = Poly([0; 1])                                                                                 
+    const x = Poly([0; 1])   
     for i = 1:n
         P = 2x*P - polyder(P)
     end
@@ -377,13 +349,11 @@ function compute_hermite_polynomial(n)
 end
 function χ(kx0, ky, α, β)
     l = sqrt(2π*α)
-
     sum = zero(Complex{Float64})
     for j in -20:20 #truncate the sum
         H = polyval(compute_hermite_polynomial(β), kx0/l + j*l)
         sum += exp(-im*ky*j) * exp(-(kx0 + j*l^2)^2/(2l^2)) * H
     end
-
     sum
 end
 
